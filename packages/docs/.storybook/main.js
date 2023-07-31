d07f34d790461070c63f731b1855ecf5aaa12244
Vinicius Martins <vm3090474@gmail.com>
2023-07-31 16:31:04 -0300

    feat: add config to deploy

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -29,5 +29,12 @@ const config = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/design-system/";
+    }
+
+    return config;
+  },
 };
 export default config;
